3829b71775f977740a68249409504ba17800664f
David Gonzales <dgonzalesn@pucp.edu.pe>
2022-06-06 20:09:19 -0500

    Avance del DAO

---
+++ src/main/java/com/example/lab07/Daos/SakilaDao.java
@@ -0,0 +1,21 @@
+package com.example.lab07.Daos;
+
+import java.util.ArrayList;
+
+public class SakilaDao {
+    private String user = "root";
+    private String pass = "root";
+    private String url = "jdbc:mysql://localhost:3306/sakila";
+
+    public ArrayList<BActor> listarActor(){
+
+    }
+    public ArrayList<BCategory> listarCategoria(){
+
+    }
+
+    public ArrayList<BFilm> listarPelicula(){
+
+    }
+
+}
