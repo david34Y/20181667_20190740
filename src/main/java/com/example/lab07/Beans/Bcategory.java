4d3c8d20edd170c65c2c2d93c1a3495ca481baa5
Niurka Carrión <niurka.carrion@pucp.edu.pe>
2022-06-06 20:36:45 -0500

    beans

--- src/main/java/com/example/lab07/Beans/Bcategory.java
+++ src/main/java/com/example/lab07/Beans/Bcategory.java
@@ -4,4 +4,28 @@ public class Bcategory {
     private int category_id;
     private String name;
     private int cantidad;
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
--- src/main/java/com/example/lab07/Beans/Bfilm.java
+++ src/main/java/com/example/lab07/Beans/Bfilm.java
@@ -4,4 +4,28 @@ public class Bfilm {
     private int film_id;
     private String tittle;
     private int cantidad;
+
+    public int getFilm_id() {
+        return film_id;
+    }
+
+    public void setFilm_id(int film_id) {
+        this.film_id = film_id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 }
