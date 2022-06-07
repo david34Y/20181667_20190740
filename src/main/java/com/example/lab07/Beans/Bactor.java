8ef030fd55cbb5117a2104c080fcfd55f0819b7e
Niurka Carrión <niurka.carrion@pucp.edu.pe>
2022-06-06 20:37:38 -0500

    beans

--- src/main/java/com/example/lab07/Beans/Bactor.java
+++ src/main/java/com/example/lab07/Beans/Bactor.java
@@ -4,5 +4,19 @@ public class Bactor {
     private int actor_id;
     private String name;
 
+    public int getActor_id() {
+        return actor_id;
+    }
 
+    public void setActor_id(int actor_id) {
+        this.actor_id = actor_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
